e947bee57d3c8aa07432b09b33195509e1dace19
Romain <rmtestard@gmail.com>
2018-11-04 17:31:19 +0100

    sgboot setup, getall & getbyid

---
+++ src/main/java/com/romain/springboot/starter/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.romain.springboot.starter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String saysHi() {
+		return "Hi";
+	}
+
+}
---
+++ src/main/java/com/romain/springboot/starter/topic/TopicService.java
@@ -0,0 +1,23 @@
+package com.romain.springboot.starter.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	List<Topic> topics = Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework Description"),
+			new Topic("java", "Core Java", "Core Java Description"),
+			new Topic("Javascript", "Core javascript", "Javascript Description")
+			);
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+}
